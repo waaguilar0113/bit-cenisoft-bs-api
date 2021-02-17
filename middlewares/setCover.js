76c143696249113235f6f90d885b0a75eb5a86da
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-17 13:03:34 -0500

    Removed unused constant.

--- middlewares/setCover.js
+++ middlewares/setCover.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
   if (req.file) {
-    const parts = req.file.originalname.split('.')
     req.body.cover = `${req.protocol}://${req.get("host")}/${req.file.destination}${req.file.filename}`
     next()
   }
