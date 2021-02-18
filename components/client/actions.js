32a9c3ae00678bc52ea3739c1fed5cbd637addc6
waaguilar0113 <waaguilar@serdan.com.co>
2021-02-17 23:27:05 -0500

    Agregar api clientes

--- components/client/actions.js
+++ components/client/actions.js
@@ -1,10 +1,11 @@
 const Client = require('./model')
 
+// metodo creacion de cliente
 const createClient = (req, res) => {
   const newClient = new Client(req.body)
   newClient.save((error, clientSaved) => {
     if (error) {
-      console.error('Error saving client ', error)
+      console.error('Error al guardar cliente ', error)
       res.status(500).send(error)
     } else {
       res.send(clientSaved)
@@ -12,4 +13,47 @@ const createClient = (req, res) => {
   })
 }
 
-module.exports = { createClient }
+// Consulta Id CLiente
+const consul_Id_Cliente = (id, res) => {
+	Client.findById(id.params.id, (errors, cliente) => {
+		if (errors) {
+			res.status(500).send(error)
+		}else if (cliente){
+			res.status(200).send(cliente)
+		}else{
+			res.status(404).send({message : ' Cliente no encontrado'})	
+		}
+	})
+}
+
+// Consulta Clientes
+const consul_Clientes = (req, res) => {
+  let query = req.query
+  if (req.query.name) {
+    query = { name: new RegExp(`.${req.query.name}.`, 'i') }
+  }
+  Client.find(query, (error, clientes) => {
+    if (error) {
+      res.status(500).send(error)
+    }else if (clientes) {
+      res.status(200).send({message : ' Datos Cliente ', clientes : clientes})
+    }else {
+      res.status(404).send({message : ' Clientes no encontrado'})
+    }
+  })
+}
+
+// Eliminar Cliente
+const eliminar_Cliente = (id, res) => {
+	Client.findByIdAndDelete(id.params.id, (error, result) => {
+		if (error) {
+			res.status(500).send(error)
+		} else if (result) {
+			res.status(200).send({message : ' Cliente Eliminado',result  : result })
+		}else {
+			res.status(204).send({message : ' Cliente no encontrado'})
+		}
+	})
+}
+
+module.exports = { createClient, consul_Id_Cliente, consul_Clientes, eliminar_Cliente }
--- components/client/routes.js
+++ components/client/routes.js
@@ -1,23 +1,33 @@
 const express = require('express')
+const Client = require('./model')
 const router = express.Router()
-const { createClient } = require('./actions')
+const { createClient, consul_Id_Cliente, consul_Clientes, eliminar_Cliente } = require('./actions')
 
-// GET by ID
-router.get('/:id', (req, res) => {
-  res.send({})
-})
+
+// Get Consulta Id CLiente
+router.get('/consul_Id_Cliente/:id', consul_Id_Cliente)
+
+// Get Consulta Clientes
+router.get('/consul_Clientes', consul_Clientes)
 
 // POST Create a Client
-router.post('/', createClient)
+router.post('/crear_Cliente', createClient)
 
 // PUT Update a Client's info
-router.put('/:id', (req, res) => {
-  res.send({})
+router.put('/modificar/:id', (id,res) =>{
+
+	Client.updateOne({ _id: id.params.id }, id.body, (error, result) => {
+		if (error) {
+			res.status(422).send(error)
+		} else if (result) {
+			res.status(200).send({message : ' Cliente Actualizado',result  : result })
+		}else {
+			res.status(404).send({message : ' Cliente no encontrado'})
+		}
+	})
 })
 
-// DELETE by ID
-router.delete('/:id', (req, res) => {
-  res.send('Cliente deleted successfully!')
-})
+// Eliminar Cliente
+router.delete('/eliminar_Cliente/:id',eliminar_Cliente)
 
 module.exports = router
